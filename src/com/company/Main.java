a382cb7bc5955b947ccb5478adfb1e167e16b75e
AngelLopez <uwu@awa.owo>
2020-12-11 14:34:20 +0100

    Anadido ya queda menos

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Quiero que lleguen las vacaciones");
+        System.out.println("Ya queda menos");
     }
 }
